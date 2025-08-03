9b0e3a8b98a4cf1d417db6f18b7de73295924eec
barkurguru <barkurguru@yahoo.co.in>
2025-08-03 15:05:20 +0530

    changes done for hostname

--- src/main/java/com/k8s/K8sApplication.java
+++ src/main/java/com/k8s/K8sApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.*;
 
 import java.util.UUID;
 
@@ -13,8 +14,8 @@ public class K8sApplication {
 
 
 	@RequestMapping("/")
-	public String home() {
-		return UUID.randomUUID().toString();
+	public String home() throws Exception {
+		return UUID.randomUUID().toString() +" IP: <<>>> "+InetAddress.getLocalHost().getHostName();
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(K8sApplication.class, args);
